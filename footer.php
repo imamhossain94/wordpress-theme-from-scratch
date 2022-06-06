926734375dd7f3545634d79a459b8a4b9c95ee76
imammasleap <imam@masleap.io>
2022-06-06 14:11:58 +0600

    feat : WordPress Theme Development Live Session + Q&A lession completed

--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
-    <footer>
-        <p>This is my footer</p>
-        <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
-    </footer>
-    
-    <?php wp_footer(); ?>
+        <footer>
+            <p>This is my footer</p>
+            <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+        </footer>   
 
+    </div><!-- .container -->
+    <?php wp_footer(); ?>
     </body>
 </html>
--- header.php
+++ header.php
@@ -16,5 +16,42 @@
 	
 	<body <?php body_class( $awesome_classes ); ?>>
 
-        <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
+        <div class="container">
+            <div class="row">
+				
+				<div class="col-xs-12">
+					
+                
+
+                <nav class="navbar navbar-default">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Awesome Theme</a>
+                        </div>
+
+
+                        <?php
+                            wp_nav_menu(array(
+                                'theme_location' => 'primary',
+                                'container' => 'false',
+                                'menu_class' => 'nav navbar-nav navbar-right',
+                            ));
+                        ?>
+
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                    </nav>
+
+
+				</div>
+				
+			</div>
+        
         <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="">
