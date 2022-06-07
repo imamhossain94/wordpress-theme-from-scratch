431e0f5bb09829d6fed640c24ebede65b389e8eb
imammasleap <imam@masleap.io>
2022-06-07 19:26:26 +0600

    feat: Part 13: Create and manage the Pagination in your blog loop lesson completed

--- index.php
+++ index.php
@@ -46,34 +46,44 @@
 
             <?php
 
-            if( have_posts() ): $i = 0;
+            if (have_posts()):
+            $i = 0;
 
-                while( have_posts() ): the_post(); ?>
+            while (have_posts()): the_post(); ?>
 
-                    <?php
-                    if($i==0): $column = 12;
-                    elseif($i > 0 && $i <= 2): $column = 6; $class = ' second-row-padding';
-                    elseif($i > 2): $column = 4; $class = ' third-row-padding';
-                    endif;
-                    ?>
+                <?php
+                if ($i == 0): $column = 12;
+                elseif ($i > 0 && $i <= 2): $column = 6;
+                    $class = ' second-row-padding';
+                elseif ($i > 2): $column = 4;
+                    $class = ' third-row-padding';
+                endif;
+                ?>
 
-                    <div class="col-xs-<?php echo $column; echo $class; ?>">
-                        <?php if( has_post_thumbnail() ):
-                            $urlImg = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
-                        endif; ?>
-                        <div class="blog-element" style="background-image: url(<?php echo $urlImg; ?>);">
+                <div class="col-xs-<?php echo $column;
+                echo $class; ?>">
+                    <?php if (has_post_thumbnail()):
+                        $urlImg = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()));
+                    endif; ?>
+                    <div class="blog-element" style="background-image: url(<?php echo $urlImg; ?>);">
 
-                            <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
+                        <?php the_title(sprintf('<h1 class="entry-title"><a href="%s">', esc_url(get_permalink())), '</a></h1>'); ?>
 
-                            <small><?php the_category(' '); ?></small>
-                        </div>
+                        <small><?php the_category(' '); ?></small>
                     </div>
+                </div>
 
-                    <?php $i++; endwhile;
+                <?php $i++; endwhile; ?>
 
-            endif;
+            <div class="col-xs-6 text-left">
+                <?php next_posts_link('« Older Posts'); ?>
+            </div>
+            <div class="col-xs-6 text-right">
+                <?php previous_posts_link('Newer Posts »'); ?>
+            </div>
 
-            ?>
+            <?php endif; ?>
+
         </div>
 
     </div>
